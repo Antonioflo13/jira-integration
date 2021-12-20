6c33022eaec06b8a454f3114f101539f0dd389a0
Antonioflo13 <floreantonio@hotmail.it>
2021-12-20 10:14:28 +0100

    first commit

---
+++ jira.js
@@ -0,0 +1,58 @@
+// import required essentials
+const express = require("express");
+// create new router
+const router = express.Router();
+
+router.post("/", function (req, res) {
+  var Client = require("node-rest-client").Client;
+  client = new Client();
+
+  // Provide user credentials, which will be used to log in to JIRA.
+  var loginArgs = {
+    headers: {
+      Authorization: "Basic YS5mbG9yZTpNaW1tYTEzMDE5Mg==",
+      "Content-Type": "application/json",
+    },
+  };
+  client.post(
+    "http://my.octavianlab.com/jira/rest/auth/1/session",
+    loginArgs,
+    function (data, response) {
+      if (response.statusCode == 200) {
+        var key = response.rawHeaders[23].substring(0, 10);
+        var id = response.rawHeaders[23].substring(11, 43);
+
+        res.json({
+          key: key,
+          id: id,
+        });
+        // Get the session information and store it in a cookie in the header
+        var searchArgs = {
+          headers: {
+            // Set the cookie from the session information
+            cookie: key + "=" + id,
+            "Content-Type": "application/json",
+          },
+          data: {
+            // Provide additional data for the JIRA search. You can modify the JQL to search for whatever you want.
+            jql: "type=Bug AND status=Closed",
+          },
+        };
+        // Make the request return the search results, passing the header information including the cookie.
+        client.get(
+          "http://my.octavianlab.com/jira/rest/api/2/issue/VUE-239",
+          searchArgs,
+          function (searchResult, response) {
+            console.log("status code:", response.statusCode);
+            console.log("search result:", searchResult);
+            res.json(searchResult);
+          }
+        );
+      } else {
+        throw "Login failed :(";
+      }
+    }
+  );
+});
+
+module.exports = router;
