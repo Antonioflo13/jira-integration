dedcc2cbfa9608da71fe29f89f464f5ea4499a2d
a.flore <a.flore@octavianlab.com>
2021-12-23 15:41:44 +0100

    set cookie

--- jira.js
+++ jira.js
@@ -1,13 +1,9 @@
-// import required essentials
 const express = require("express");
-// create new router
 const router = express.Router();
+var Client = require("node-rest-client").Client;
+client = new Client();
 
-router.post("/", function (req, res) {
-  var Client = require("node-rest-client").Client;
-  client = new Client();
-
-  // Provide user credentials, which will be used to log in to JIRA.
+router.post("/login", function (req, res) {
   var loginArgs = {
     headers: {
       Authorization: "Basic YS5mbG9yZTpNaW1tYTEzMDE5Mg==",
@@ -21,21 +17,8 @@ router.post("/", function (req, res) {
       if (response.statusCode == 200) {
         var key = response.rawHeaders[23].substring(0, 10);
         var id = response.rawHeaders[23].substring(11, 43);
-
-        res.cookie(key + "=" + id);
+        res.cookie(key,id, {maxAge: 9000000000, httpOnly: true});
         res.json({ cookie: key + "=" + id });
-        // Get the session information and store it in a cookie in the header
-        var searchArgs = {
-          headers: {
-            // Set the cookie from the session information
-            cookie: key + "=" + id,
-            "Content-Type": "application/json",
-          },
-          data: {
-            // Provide additional data for the JIRA search. You can modify the JQL to search for whatever you want.
-            jql: "type=Bug AND status=Closed",
-          },
-        };
       } else {
         throw "Login failed :(";
       }
@@ -44,17 +27,16 @@ router.post("/", function (req, res) {
 });
 
 router.get("/search", function (req, res) {
-  var Client = require("node-rest-client").Client;
-  client = new Client();
-
   var key = "JSESSIONID";
   var id = req.cookies["JSESSIONID"];
+  console.log(id);
+
   // Make the request return the search results, passing the header information including the cookie.
   client.get(
     `http://my.octavianlab.com/jira/rest/api/2/${req.query.type}/${req.query.id}`,
     {
       headers: {
-        cookie: key + "=" + id,
+        cookie: key + "=" + 'EF62BD85691A13B63CEF413047E4EFC3',
         "Content-Type": "application/json",
       },
       data: {
--- server.js
+++ server.js
@@ -2,17 +2,17 @@ const http = require("http");
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 const jiraLogin = require("./jira");
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: "http://localhost:8080" }));
-app.use("/login", jiraLogin);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use("/", function (req, res) {
-  res.send("node works :-)");
-});
+app.use("/", jiraLogin);
 
 const server = http.createServer(app);
 const port = 3000;
